c2bdcfdde8e9f2290e4fe75b96814fc9efddd8fd
Hassan <Hassan.Alhawari@jaemacom.de>
2023-12-02 20:35:49 +0100

    preprocess file added

--- PreprocessingDataSet.py
+++ PreprocessingDataSet.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 
-file_path = "C:\\Users\\HassanAlhawari\\Downloads\\sampleSubmission.csv"
+file_path = "sampleSubmission.csv"
 dataset = pd.read_csv(file_path, sep=";")
 selected_columns = dataset[["LARCENY.THEFT", "ASSAULT", "DRUG.NARCOTIC", "VEHICLE.THEFT", "BURGLARY"]]
 
